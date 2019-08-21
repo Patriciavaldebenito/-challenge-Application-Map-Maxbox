28a2ef67aec7940c93bf9417ddf18693253e5054
Patriciavaldebenito <patricia.valdebenito@usach.cl>
2019-08-21 00:50:12 -0400

    Inicializacion de mapa en div contenedor con coordenadas iniciales

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { DataService } from './data.service';
+import * as mapboxgl from 'mapbox-gl';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [DataService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'job-Application-pv';
+  // setting 
+  map: mapboxgl
+  style = 'mapbox://styles/mapbox/streets-v11';
+  lat = 39.390897;
+  lng = -99.066067;
+
+
+  constructor (public dataService: DataService){
+    (mapboxgl as any ).accessToken = environment.mapbox.accessToken;
+  }
+
+  ngOnInit() {
+    var map = new mapboxgl.Map({
+      container: 'map', // Specify the container ID
+      style: 'mapbox://styles/mapbox/streets-v11', // Specify which map style to use
+      center: [-74.016133, 40.640771], // Specify the starting position
+      zoom: 10, // Specify the starting zoom
+    });
+   // this.initializeMap();
+  }
 }
+
+  
+
+
+
+
+
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   mapbox: { 
-    accessToken: 'pk.eyJ1IjoicGF0cmljaWF2YWxkZWJlbml0byIsImEiOiJjanphYzVpcmswMGRrM2VxcmZ6ZXdwdmZ2In0.lFGGEp9Q44FcIjW6wy2W1A' 
+    accessToken: 'pk.eyJ1IjoicGF0cmljaWF2YWxkZWJlbml0byIsImEiOiJjanprcXo4NjIwbnJpM2RvYTJ1ZDV4a21iIn0.zuW9k_uTz8UVL_4WYzFSeg' 
    },
 
   apiURL:'https://raw.githubusercontent.com/digital-generation/generation-take-home-intern/master/src/store_directory.json',
